686ee771cd11868b5d976f20863613ca903ff3e0
Juan Jose <156539463+Arcademys@users.noreply.github.com>
2025-08-28 23:28:00 -0300

    actulizacion

---
+++ Actividad de cierre de la unidad 3 - Estructuras Condicionales/07_ ultima_vocal.py
@@ -0,0 +1,10 @@
+
+
+
+
+data = input("Escribe algo:")
+
+if data[-1] == "a" or data[-1] == "e" or data[-1] == "i" or data[-1] == "o" or data[-1] == "u" :
+    print(f"{data}!")
+else:
+    print(f"{data}")

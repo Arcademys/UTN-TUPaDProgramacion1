83596737e65f87e2ae102137b059a898698a08d8
Juan Jose <156539463+Arcademys@users.noreply.github.com>
2025-09-09 20:52:45 -0300

    Update 03_Sumando_al_grupo.py

--- Practico 4 Estructuras repetitivas/03_Sumando_al_grupo.py
+++ Practico 4 Estructuras repetitivas/03_Sumando_al_grupo.py
@@ -1,10 +1,10 @@
 nro1 = int(input("Ingresa un numero: "))
 nro2 = int(input("ingresa otro numero: "))
+menor, mayor = sorted([nro1,nro2])
 suma = 0
 
-if nro1 < nro2:
-    
-    for i in range (nro1, nro2 -1):
+
+
+for i in range (menor +1, mayor ):
         suma +=  i
-    
-    print(f"{suma}")
+print(f"Suma entre {menor} y {mayor} es: {suma}")

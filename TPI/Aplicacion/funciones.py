f3fef921f2a3d7bda76d37f8f991887d8345b915
Juan Jose Benitez <juanjolifeargentina@gmail.com>
2025-11-02 19:34:39 -0300

    se agregan funciones y mainconsole

---
+++ TPI/Aplicacion/funciones.py
@@ -0,0 +1,13 @@
+leer_csv(nombre_archivo)
+
+guardar_csv(lista_paises)
+
+agregar_pais(lista_paises, datos)
+
+buscar_pais(lista_paises, nombre)
+
+filtrar_por_continente(lista_paises, continente)
+
+ordenar(lista_paises, clave, ascendente=True)
+
+calcular_estadisticas(lista_paises)
